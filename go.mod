33e0cfc35c5010c47a75608606aa06c18d44dc1a
akshay <ask029849@gmail.com>
2024-03-22 11:43:30 +0530

    slight changes

--- go.mod
+++ go.mod
@@ -3,15 +3,32 @@ module github.com/akshay0074700747/email-service
 go 1.21.5
 
 require (
-	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/joho/godotenv v1.5.1
 	google.golang.org/protobuf v1.32.0
 )
 
 require (
+	github.com/IBM/sarama v1.43.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.6.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240108191215-35c7eff3a6b1 // indirect
 	google.golang.org/grpc v1.60.1 // indirect
--- kafka/kafka.go
+++ kafka/kafka.go
@@ -4,10 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 
+	"github.com/IBM/sarama"
 	"github.com/akshay0074700747/email-service/email"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 type SendMail struct {
@@ -24,30 +23,67 @@ func Getmailer(mail *email.SMTPConfig) {
 }
 
 func StartServing() {
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers":        "host.docker.internal:9092",
-		"group.id":                 "EmailConsumers",
-		"auto.offset.reset":        "earliest",
-		"allow.auto.create.topics": true})
+	// consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+	// 	"bootstrap.servers":        "host.docker.internal:9092",
+	// 	"group.id":                 "EmailConsumers",
+	// 	"auto.offset.reset":        "earliest",
+	// 	"allow.auto.create.topics": true})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// topic := "Emailsender"
+	// err = consumer.Assign([]kafka.TopicPartition{
+	// 	{
+	// 		Topic:     &topic,
+	// 		Partition: 0,
+	// 		Offset:    kafka.OffsetStored,
+	// 	},
+	// })
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// for {
+	// 	ev := consumer.Poll(10)
+	// 	switch e := ev.(type) {
+	// 	case *kafka.Message:
+	// 		go func() {
+	// 			log.Println(string(e.Value))
+	// 			log.Println("above is the value")
+	// 			var res SendMail
+	// 			err := json.Unmarshal(e.Value, &res)
+	// 			if err != nil {
+	// 				log.Println(err)
+	// 			}
+	// 			log.Println("about to assert...")
+	// 			fmt.Println(res)
+	// 			if err = Mailer.SendMessage(res.Email, res.Message); err != nil {
+	// 				log.Println(err)
+	// 			}
+	// 		}()
+	// 	case kafka.Error:
+	// 		fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+	// 	}
+	// }
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Metadata.AllowAutoTopicCreation = true
+	config.Consumer.Offsets.AutoCommit.Enable = true
+	consumer, err := sarama.NewConsumer([]string{"host.docker.internal:9092"}, config)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error creating consumer: %v", err)
 	}
-	topic := "Emailsender"
-	err = consumer.Assign([]kafka.TopicPartition{
-		{
-			Topic:     &topic,
-			Partition: 0,
-			Offset:    kafka.OffsetStored,
-		},
-	})
+	defer consumer.Close()
+
+	partitionConsumer, err := consumer.ConsumePartition("Emailsender", 0, sarama.OffsetNewest)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalf("Error creating partition consumer: %v", err)
 	}
+	defer partitionConsumer.Close()
+
 	for {
-		ev := consumer.Poll(10)
-		switch e := ev.(type) {
-		case *kafka.Message:
+		select {
+		case e := <-partitionConsumer.Messages():
 			go func() {
 				log.Println(string(e.Value))
 				log.Println("above is the value")
@@ -62,8 +98,9 @@ func StartServing() {
 					log.Println(err)
 				}
 			}()
-		case kafka.Error:
-			fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+
+		case err := <-partitionConsumer.Errors():
+			log.Printf("Error consuming message: %v", err)
 		}
 	}
 }
