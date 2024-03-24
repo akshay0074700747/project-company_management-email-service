5c8ff5d3b7ed7e3ba53ff1ab2fdc3180c8e06145
akshay <ask029849@gmail.com>
2024-03-24 10:54:58 +0530

    deployig to k8s

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -74,7 +74,7 @@ func StartServing() {
 	var consumer sarama.Consumer
 	var err error
 	for i := 0; i < 8; i++ {
-		consumer, err = sarama.NewConsumer([]string{"apache-kafka-service:19092"}, config)
+		consumer, err = sarama.NewConsumer([]string{"apache-kafka-service:29092"}, config)
 		if err != nil {
 			if i == 7 {
 				log.Fatal("Closingg: %v", err)
